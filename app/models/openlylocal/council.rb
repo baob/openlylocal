391c2fd0cd38e0c6d0db014b96bbcc6d58b55fec
baob <coder@onesandthrees.com>
2010-12-10 01:08:32 +0000

    refinements, no specs

--- app/models/openlylocal/council.rb
+++ app/models/openlylocal/council.rb
@@ -6,7 +6,11 @@ module Openlylocal
 
   class Council
   
-    attr_accessor :xml_data, :name, :openlylocal_url, :wikipedia_url, :address
+    attr_accessor :xml_data, :id, :name, :openlylocal_url, :wikipedia_url, :address, :normalised_title, :url,
+                  :telephone, :country, :region
+
+    OL_FILENAME = File.expand_path(File.dirname(__FILE__) + "/../../../files/openlylocal_councils.xml")
+    OL_COUNCILS_URL = "http://openlylocal.com/councils.xml"
 
     def initialize(council_node)
       self.xml_data = council_node
@@ -14,31 +18,29 @@ module Openlylocal
       self.openlylocal_url = council_node.elements['openlylocal-url'].text
       self.wikipedia_url = council_node.elements['wikipedia-url'].text
       self.address = council_node.elements['address'].text
+      self.id = council_node.elements['id'].text
+      self.normalised_title = council_node.elements['normalised-title'].text
+      self.url = council_node.elements['url'].text
+      self.telephone = council_node.elements['telephone'].text
+      self.country = council_node.elements['country'].text
+      self.region = council_node.elements['region'].text
       self
     end
 
-    filename = File.expand_path(File.dirname(__FILE__) + "/../../../files/openlylocal_councils.xml")
-    
-    puts "------------"+filename
-    
-    begin
-      file = File.new(filename)
-    rescue Errno::ENOENT
-      puts "------------ get "
-      
-      url = URI.parse('http://openlylocal.com/councils.xml')
+    def self.fetch_file
+      url = URI.parse(OL_COUNCILS_URL)
       req = Net::HTTP::Get.new(url.path)
       res = Net::HTTP.start(url.host, url.port) {|http|
         http.request(req)
       }
-  
-      puts "------------ get 2"
-      
-      File.open(filename, 'w') {|f| f.write(res.body) }
-      puts "------------ get 3 "
-      
-      file = File.new(filename)
-      
+      File.open(OL_FILENAME, 'w') {|f| f.write(res.body) }
+    end
+
+    begin
+      file = File.new(OL_FILENAME)
+    rescue Errno::ENOENT
+      fetch_file
+      file = File.new(OL_FILENAME)
     end 
     
     council_doc = REXML::Document.new(file)
@@ -47,9 +49,23 @@ module Openlylocal
     end
     
   
-    def self.find(name)
+    def self.find_by_name(name)
       @@councils.detect{ |c| c.name == name } 
     end
-  end
+
+    def self.find(id) # find on openly local's own id, takes string or integer
+      match_id = id.is_a?(Fixnum) ? id.to_s : id
+      @@councils.detect{ |c| c.id == match_id } 
+    end
+    
+    def self.all
+      @@councils
+    end
+
+    def self.count
+      @@councils.size
+    end
+    
+  end # class Council
 
 end
